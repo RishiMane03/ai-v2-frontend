29a456a8559a0f691ebf9713bea2b87e81cb75b2
Rishi Mane <rishimane03@gmail.com>
2024-04-03 09:02:34 +0530

    another commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import Summary from './Components/Summary/Summary';
+
+// Pages
+import LandingPage from './Components/Landingpage/LandingPage';
 import SignInOut from './Components/SignInOut/SignInOut';
+import Summary from './Components/Summary/Summary';
+import Code from './Components/Code/Code';
+import Chat from './Components/Chat/Chat';
+import Pdf from './Components/Pdf/Pdf';
 
 function App() {
 
   return (
     <div>
-      Welcome to App.js in REACT
       <ToastContainer />
       <Router>
           <Routes>
+            <Route path='/' element={<LandingPage/>}/> 
             <Route path='/aihubAuth' element={<SignInOut/>}/> 
-            <Route path='/aihubSummary' element={<Summary/>}/> 
+            <Route path='/summaryHub' element={<Summary/>}/> 
+            <Route path='/codeHub' element={<Code/>}/> 
+            <Route path='/chatHub' element={<Chat/>}/> 
+            <Route path='/pdfHub' element={<Pdf/>}/> 
           </Routes>
         </Router>
     </div>
